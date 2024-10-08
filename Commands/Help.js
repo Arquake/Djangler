4822ab405d913e37ba0f0d2358f86cce338d782e
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-10-08 23:06:53 +0200

    PATCH
    minor patch inside HELP.js for some spaces between commands

--- Commands/Help.js
+++ Commands/Help.js
@@ -5,17 +5,16 @@ export default class Help {
         if(commands.length === 0) {
             console.log (chalk.green(
                 'help : list all of the available commands' +
-                '' +
+                '\n\n' +
                 'new : ask you the name of the new project and generate it' +
-                '' +
+                '\n\n' +
                 's : run a local dev server' +
-                '' +
+                '\n\n' +
                 'new : generate a new project \n\n' +
                 'g type -flag --component_name : generate a new component \n' +
                 '\ta : generate a new app \n' +
                 /*'\tv -app_name : generate a new view with its template\n' +*/
-                '' +
-                ''
+                '\n\n'
             ));
         }
     }
