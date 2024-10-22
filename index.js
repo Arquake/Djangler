bb8c04acda98ed496e7836e4317def46eb378c87
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-10-22 11:04:48 +0200

    PUSH
    Added a version checker with `dja v`

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import ConsoleLogs from "./console-logs/ConsoleLogs.js";
 import CreateApp from "./Commands/CreateApp.js";
 import Serve from "./Commands/Serve.js";
 import ManageMigration from "./Commands/file-generator/ManageMigration.js";
+import chalk from "chalk";
+import {dirname} from "./dirname.js";
+import fs from "fs";
 
 try {
     switch (process.argv[2]) {
@@ -23,6 +26,10 @@ try {
             break;
         case "m":
             ManageMigration.handleCommand(process.argv.slice(3));
+            break;
+        case "v":
+            const version = fs.readFileSync(`${dirname}/package.json`, "utf8").match(/"version": "([0-9\\.]*)"/)[1];
+            console.log(chalk.yellow(`Current version of Djangler : ${version}`))
             break;
         default:
             throw new Error(`Error: Command don\'t exist`)
